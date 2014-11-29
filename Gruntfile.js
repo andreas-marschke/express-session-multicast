ac04441a30738f9f76ea4478f61b35f2e39f23b4
Andreas Marschke <andreas.marschke@gmail.com>
2014-11-30 00:11:11 +0100

    Add grunt linting and basic scaffold for later unit-testing

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+	pkg: grunt.file.readJSON("package.json"),
+	eslint: {
+	    options: {
+		format: "compact"
+	    },
+	    target: [
+		"Gruntfile.js",
+		"index.js",
+		"lib/store.js",
+		"lib/serialization.js"
+	    ]
+	},
+	mochaTest: {
+	    test: {
+		options: {
+		    reporter: "tap",
+		    quiet: false,
+		    clearRequireCache: true,
+		    gc: true
+		},
+		src: [
+		    "tests/index.js"
+		]
+	    }
+	}
+    });
+
+    grunt.loadNpmTasks("grunt-eslint");
+    grunt.loadNpmTasks("grunt-mocha-test");
+
+    grunt.registerTask("test", ["eslint", "mochaTest"]);
+    grunt.registerTask("default", ["test"]);
+};
