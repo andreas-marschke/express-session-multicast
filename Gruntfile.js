ee83ce304e12de5900dcf642508811b5939ac50b
Andreas Marschke <andreas.marschke@gmail.com>
2016-12-01 23:58:16 +0100

    Unify and cleanup syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 "use strict";
-
 module.exports = function(grunt) {
-    grunt.initConfig({
-	pkg: grunt.file.readJSON("package.json"),
-	eslint: {
-	    options: {
-		format: "compact"
-	    },
-	    target: [
-		"Gruntfile.js",
-		"index.js",
-		"lib/store.js",
-		"lib/serialization.js"
-	    ]
-	},
-	mochaTest: {
-	    test: {
-		options: {
-		    reporter: "tap",
-		    quiet: false,
-		    clearRequireCache: true,
-		    gc: true
-		},
-		src: [
-		    "test/index.js"
-		]
-	    }
-	}
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    eslint: {
+      options: {
+        configFile: ".eslintrc",
+        format: "compact"
+      },
+      src: [
+        "Gruntfile.js",
+        "index.js",
+        "lib/store.js",
+        "lib/serialization.js"
+      ]
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: "tap",
+          quiet: false,
+          clearRequireCache: true,
+          gc: true
+        },
+        src: [
+          "test/index.js"
+        ]
+      }
+    }
+  });
 
-    grunt.loadNpmTasks("grunt-eslint");
-    grunt.loadNpmTasks("grunt-mocha-test");
+  grunt.loadNpmTasks("gruntify-eslint");
+  grunt.loadNpmTasks("grunt-mocha-test");
 
-    grunt.registerTask("test", ["eslint", "mochaTest"]);
-    grunt.registerTask("default", ["test"]);
+  grunt.registerTask("test", ["eslint", "mochaTest"]);
+  grunt.registerTask("default", ["test"]);
 };
